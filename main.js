35838d9682e9dd37e4d35ce7177c9bdc432e6079
Norwood Henderson <norwoodmhii@gmail.com>
2023-08-29 14:03:21 -0400

    first comment

---
+++ main.js
@@ -0,0 +1,52 @@
+/*
+const wardrobe = {
+    height: 80,
+    manufacturer: "Killibrew & Sons",
+    contents: ["Dress shirt", "Jeans", "Suit", "Skirt", "Tennis shoes"],
+    depth: 38,
+    width: 50
+}
+wardrobe.material = "Cedar"
+console.log(wardrobe);
+*/
+
+/*
+const empireStateBuilding = {
+    stories: 103,
+    height: 1453,
+    address: "350 Fifth Avenue, Manhattan, New York 10118",
+    squareFeet: 2768591,
+    constructionDate: 1931,
+    cost: 40948900,
+    owner: "Empire State Realty Trust",
+    eastWestLength: 424,
+    northSouthLength: 187,
+    architect: "Shreve, Lamb & Harmon"
+}
+let address = "350 Fifth Avenue, Manhatten, New Your 10118";
+let constructionDate = "1931";
+let cost = "$40,948,900";
+let owner = "Empire State Realty Trust";
+let architect = "Shreve, Lamb & Harmon";
+
+console.log(empireStateBuilding.stories);
+console.log(empireStateBuilding.height);
+console.log(empireStateBuilding.squareFeet);
+console.log(empireStateBuilding.eastWestLength);
+console.log(empireStateBuilding.northSouthLength);
+
+console.log([address], [constructionDate], [cost], [owner], [architect])
+*/
+
+const nashvilleSoftwareSchool = {
+    founded: 2012,
+    director: "John Wark",
+    instructors: {
+        fullTime: ["Jisie", "Brenda", "Steve", "Joe", "Andy"],
+        partTime: ["Zoe", "Nathan"]
+    },
+    address: "500 Interstate Blvd. S"
+}
+
+console.log({partTime: [0, 1, 2, 3, 4], fullTime: [0, 1]});
+console.log({fullTime: [4] partTime: [0]});
